83eec8f91f8b2d9a375fca687ab4a71937bf4b60
Goli <Goli@Goli-HP>
2022-06-06 12:18:51 +0530

    fifth commit

---
+++ assignment2/src/assignment2/Patterns.java
@@ -0,0 +1,17 @@
+package assignment2;
+
+public class Patterns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i,j;
+      int n=10;
+      for(i=1;i<=5;i++)
+      {
+    	  for(j=1;j<=i;j++)
+    		  System.out.println(i);
+    	      System.out.println("");
+      }
+	}
+
+}
